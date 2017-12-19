59d4869784b2f8ca906ae686ca20bf553e6881e0
Ravi Shankar Pendyala <ravishankar.pendyala@gmail.com>
2017-12-19 13:21:23 +0530

    Changed through pyhton code

---
+++ Python Code/Python Code.py
@@ -0,0 +1,12 @@
+import os
+import subprocess
+
+def uploadrepo(base_path ,file_path, github_path):
+	os.chdir(base_path)
+	subprocess.call('git clone '+github_path)
+	path = os.path.join(base_path, file_path)
+	print(path)
+	os.chdir(path)
+	subprocess.call('git add .')
+	subprocess.call('git commit -m "Changed through pyhton code"')
+	subprocess.call('git push origin master')
