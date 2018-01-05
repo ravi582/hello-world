07430cbe53de9f12e7ad6473e7a6267bdd7a2087
Ravi Shankar Pendyala <ravishankar.pendyala@gmail.com>
2018-01-05 17:31:54 +0530

    Changed through pyhton code

---
+++ test/test.py
@@ -0,0 +1,7 @@
+constructor(private _http:Http){
+        this.url_get_postcodes = "http://inl00528:9090/snippet_list/";
+        this.url_getcode_by_lang_type = "http://inl00528:9090/snippets/?lang_type=";
+        this.url_getcode_by_code_title = " http://inl00528:9090/snippets/?title=";
+        this.url_getlanguages = "http://inl00528:9090/languages/";
+        
+    }
